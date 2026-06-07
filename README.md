7052f8627f0f715b3135889859dd0431540d72a3
mckennalmorgan <mckennalmorgan@outlook.com>
2026-06-06 22:36:07 -0700

    Finished main calculation.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # islenskur_skattreiknivel
-Terminal-based calculator for taxes in Iceland
+Terminal-based calculator for taxes in Iceland. Simply enter your monthly income to see what your net salary after taxes will be. Note that if the tax brackets, personal tax credit, or pension amounts change, you will need to update these in the 'calculator.py' file.
---
+++ calculator.py
@@ -0,0 +1,37 @@
+def calculate_monthly_tax(
+    monthly_income,
+    pension_credit=0.04,
+    personal_credit=72492
+):
+    tax_brackets = [
+        {"limit": 498122, "rate": 0.3149},
+        {"limit": 1398450, "rate": 0.3799},
+        {"limit": float('inf'), "rate": 0.4599}
+    ]
+    taxable_income = monthly_income * (1 - pension_credit)
+    print("\n\n========================================== \n")
+    print(f"Your taxable monthly income is {taxable_income:.2f}\n")
+    print(f"Your personal tax credit is {personal_credit}.\n")
+    print("\n========================================== \n\n")
+
+
+    tax = 0
+    previous_bracket = 0 # since there is no limit below 31.49%
+    for bracket in tax_brackets:
+        taxable_amount = max(0, min(taxable_income, bracket["limit"]) - previous_bracket)
+        tax += taxable_amount * bracket["rate"]
+        print(f"Tax from {bracket['rate']*100:.2f}% bracket = {taxable_amount * bracket['rate']:.2f}\n")
+        previous_bracket = bracket["limit"]
+    tax = tax - personal_credit
+
+    print("\n\n========================================== \n")
+    print(f"Your total monthly tax is {tax:.2f}.\n")
+    print(f"Your net salary after taxes is {taxable_income - tax:.2f}\n")
+    print("\n========================================== \n\n")
+
+if __name__ == "__main__":
+    print("\n\n========================================== \n")
+    monthly_income = float(input("What is your monthly income? "))
+    print("\n==========================================")
+
+    calculate_monthly_tax(monthly_income)
